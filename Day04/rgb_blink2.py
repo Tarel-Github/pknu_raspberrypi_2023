c48a6c424b58c19ef041ee415389ae84b078ba8e
Tarel-Github <111163125+Tarel-Github@users.noreply.github.com>
2023-06-12 16:57:47 +0900

    라즈베리 파이

---
+++ Day04/rgb_blink2.py
@@ -0,0 +1,23 @@
+# LED RGB 깜빡이기
+import RPi.GPIO as GPIO
+import time
+
+red = 17; green = 27; blue = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red, GPIO.OUT)
+GPIO.output(red, False)
+GPIO.output(green, False)
+GPIO.output(blue, False)
+
+
+try:
+    while True:
+        GPIO.output(red, True)
+        time.sleep(0.5)
+
+        GPIO.output(red, False)
+
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
--- README.md
+++ README.md
@@ -27,3 +27,9 @@
 				- systemctl start mysql, status, stop, restart
 			- MySQL DB
 		- Flask 기본
+
+## 4일차
+- 라즈베리파이 학습
+	- PyQt5 설치
+	- QtDesigner 설치 및 실행확인
+	- 하드웨어 제어 - GPIO
